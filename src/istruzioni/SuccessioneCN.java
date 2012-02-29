618d01340c1f982c54ced5154a0cf2cc0b75279c
Luca Marturana <lucamarturana@gmail.com>
2012-02-29 17:22:33 +0100

    Implementazione scrittura di alcune istruzioni

--- src/istruzioni/SuccessioneCN.java
+++ src/istruzioni/SuccessioneCN.java
@@ -3,6 +3,7 @@ package istruzioni;
 public class SuccessioneCN implements N {
 	private C istruzionePrecedente;
 	private N istruzioneSuccessiva;
+	
 	public SuccessioneCN(C istruzionePrecedente, N istruzioneSuccessiva) {
 		this.istruzionePrecedente = istruzionePrecedente;
 		this.istruzioneSuccessiva = istruzioneSuccessiva;
