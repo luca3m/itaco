6adf6ca17fa0eb96f797ac16f68d2eaf1a18dc76
Luca Marturana <lucamarturana@gmail.com>
2012-03-17 14:49:13 +0100

    Eliminati i bordi nei bottoni

--- src/GUI/FinestraPrincipale.java
+++ src/GUI/FinestraPrincipale.java
@@ -80,6 +80,7 @@ public class FinestraPrincipale extends JFrame {
 	
 		
 		JButton btnNewButton = new JButton("Salva File");
+		btnNewButton.setBorder(null);
 		btnNewButton.setIcon(new ImageIcon(FinestraPrincipale.class.getResource("/img/salva copia.png")));
 		btnNewButton.setBorderPainted(false);
 		btnNewButton.addActionListener(new ActionListener() {
@@ -99,6 +100,7 @@ public class FinestraPrincipale extends JFrame {
 		contentPane.add(btnNewButton);
 		
 		JButton btnGeneraFile = new JButton("Apri File");
+		btnGeneraFile.setBorder(null);
 		btnGeneraFile.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				JFileChooser chooser = new JFileChooser();
@@ -117,6 +119,7 @@ public class FinestraPrincipale extends JFrame {
 		contentPane.add(btnGeneraFile);
 		
 		JButton btnPulisci = new JButton("");
+		btnPulisci.setBorder(null);
 		btnPulisci.setToolTipText("Compila ed esegui");
 		btnPulisci.setBorderPainted(false);
 		btnPulisci.setIcon(new ImageIcon(FinestraPrincipale.class.getResource("/img/tastoplay0.png")));
@@ -324,6 +327,7 @@ public class FinestraPrincipale extends JFrame {
 		contentPane.add(lblImg);
 		
 		JButton btnNuovoFile = new JButton("Nuovo File");
+		btnNuovoFile.setBorder(null);
 		btnNuovoFile.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				fileItaco = new FileItaco();
@@ -336,6 +340,7 @@ public class FinestraPrincipale extends JFrame {
 		contentPane.add(btnNuovoFile);
 		
 		JButton btnNewButton_1 = new JButton("");
+		btnNewButton_1.setBorder(null);
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (fileItaco.fileSalvato()) {
@@ -356,6 +361,7 @@ public class FinestraPrincipale extends JFrame {
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("");
+		btnNewButton_2.setBorder(null);
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (fileItaco.fileSalvato()) {
@@ -376,6 +382,7 @@ public class FinestraPrincipale extends JFrame {
 		contentPane.add(btnNewButton_2);
 		
 		JButton button = new JButton("");
+		button.setBorder(null);
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (fileItaco.fileSalvato()) {
@@ -396,6 +403,7 @@ public class FinestraPrincipale extends JFrame {
 		contentPane.add(button);
 		
 		JButton btnNewButton_3 = new JButton("");
+		btnNewButton_3.setBorder(null);
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (fileItaco.fileSalvato()) {
