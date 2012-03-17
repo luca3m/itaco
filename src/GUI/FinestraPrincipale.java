98d2cff683d1a9dadbb2931503b3139a109e9981
Luca Marturana <lucamarturana@gmail.com>
2012-03-17 13:12:17 +0100

    Aggiunto Look Nimbus

--- src/GUI/FinestraPrincipale.java
+++ src/GUI/FinestraPrincipale.java
@@ -35,6 +35,15 @@ public class FinestraPrincipale extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+        }
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
